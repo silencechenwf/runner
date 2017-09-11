2b21904ad3e8cd13174abdb75172d93a82219f5a
silencechenwf <18573428816@163.com>
2017-09-11 14:17:08 +0800

    Initial commit

---
+++ src/main/java/com/chinasofti/pojo/Friend.java
@@ -0,0 +1,43 @@
+package com.chinasofti.pojo;
+
+public class Friend {
+    private Integer friendId;
+
+    private String userName;
+
+    private String friend;
+
+    private Integer state;
+
+    public Integer getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(Integer friendId) {
+        this.friendId = friendId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getFriend() {
+        return friend;
+    }
+
+    public void setFriend(String friend) {
+        this.friend = friend == null ? null : friend.trim();
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/chinasofti/service/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.chinasofti.com/")
+package com.chinasofti.service;
