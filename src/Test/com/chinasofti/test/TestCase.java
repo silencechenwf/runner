3c18b96c2e4c03d06e5c13d910d67c0ec003a9bf
silencechenwf <18573428816@163.com>
2017-09-14 13:37:24 +0800

    1

--- src/Test/com/chinasofti/test/TestCase.java
+++ src/Test/com/chinasofti/test/TestCase.java
@@ -4,14 +4,18 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 /**
  * Created by Administrator on 2017/9/12.
  */
 @ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
-public class TestCase {
+public class TestCase extends AbstractJUnit4SpringContextTests {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+
+
     @Test
     public void testUpdate(){
         int counts = jdbcTemplate.update("DELETE  from userinfo");
